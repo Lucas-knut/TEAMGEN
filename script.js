a0965997092c531b0b5856012fa198f779cdf50b
Yohan Beneito <bwb.beneito.y@fondespierre.com>
2021-04-04 10:10:20 +0000

    Checkbox group added

--- script.js
+++ script.js
@@ -1,170 +1,182 @@
-//Après moults tests et recherches, je suis tombé sur un algorithme connu "le mélange de Fisher-Yates".
-//Celui ci permet de parcourir notre tableau de droite à gauche avec une boucle.
-//On prends la valeur de l'élément de notre tableau (le dernier, avant dernier et ainsi de suite) la poste dans une variable temporaire,
-//on tire un nombre aleatoire entre 0 et la taille de notre tableau et on permute les deux valeurs et on réhitère jusqu'à avoir fait ceci avec toutes les valeurs
-
+//      Creation des personnes       
 
 var perso = {
-    id : null,
-    nom : "",
-    dispo : {
-        lundi : null,
-        mardi : null,
-        mercredi : null,
-        jeudi : null,
-        vendredi : null
-    }
+    nom: "",
+    g1: false,
+    g2: false
 }
+
 var yohan = Object.assign({}, perso)
-yohan.id = 0
 yohan.nom = "Yohan"
 
 var lucas = Object.assign({}, perso)
-lucas.id = 1
 lucas.nom = "Lucas"
 
 var sidney = Object.assign({}, perso)
-sidney.id = 2
 sidney.nom = "Sidney"
 
 var maxime = Object.assign({}, perso)
-maxime.id = 3
 maxime.nom = "Maxime"
 
-
 var marylise = Object.assign({}, perso)
-marylise.id = 4
 marylise.nom = "Marylise"
 
 var tamara = Object.assign({}, perso)
-tamara.id = 5
 tamara.nom = "Tamara"
 
 var alexandre = Object.assign({}, perso)
-alexandre.id = 6
 alexandre.nom = "Alexandre"
 
-
 var juan = Object.assign({}, perso)
-juan.id = 7
 juan.nom = "Juan"
 
 var valentin = Object.assign({}, perso)
-valentin.id = 8
 valentin.nom = "Valentin"
 
 var raph = Object.assign({}, perso)
-raph.id = 9
 raph.nom = "Raph"
 
 var laurène = Object.assign({}, perso)
-laurène.id = 10
 laurène.nom = "Laurène"
 
-
 var noureddine = Object.assign({}, perso)
-noureddine.id = 11
 noureddine.nom = "Noureddine"
 
 var vincent = Object.assign({}, perso)
-vincent.id = 12
 vincent.nom = "Vincent"
 
 var maïalen = Object.assign({}, perso)
-maïalen.id = 13
 maïalen.nom = "Maïalen"
 
 var fanny = Object.assign({}, perso)
-fanny.id = 14
 fanny.nom = "Fanny"
 
-var personnes = []
-personnes.push(yohan,lucas,sidney,maxime,marylise,tamara,alexandre,juan,valentin,raph,laurène,noureddine,vincent,maïalen,fanny)
-//var personnes = ["yohan", "lucas", "sidney", "maxime", "marylise", "tamara", "alexandre", "juan", "valentin", "raph", "laurène", "noureddine", "vincent", "maïalen", "fanny"]
+//      Récupération ou envois des datas dans le localStorage
+
+//localStorage.removeItem("personnes")
+function getsetLocalStr() {
+    if (localStorage.length == 0) {
+        var personnes = []
+        personnes.push(yohan, lucas, sidney, maxime, marylise, tamara, alexandre, juan, valentin, raph, laurène, noureddine, vincent, maïalen, fanny)
+
+        localStorage.setItem("personnes", JSON.stringify(personnes))
+        return personnes
+    } else {
+        var personnes = JSON.parse(localStorage.getItem("personnes"))
+        return personnes
+    }
+}
+
+var personnes = getsetLocalStr()
+
+
+//      Récupération du DOM
 var table1 = document.getElementById("table1")
 var table2 = document.getElementById("table2")
-console.log(personnes);
-
-
-
 var btn = document.getElementById("btn")
 
-//Ajout de la partie qui génére un tableau HTML avec tous les apprenants
-var tableBase = document.getElementById("basetab")
 
+//      Ajout de la partie qui génére et affiche un tableau HTML avec tous les apprenants
+var tableBase = document.getElementById("basetab")
 personnes.forEach(element => {
     var tbr = document.createElement("tr")
-    tbr.append(element.nom)
+    var td1 = document.createElement("td")
+    td1.append(element.nom)
+    tbr.append(td1)
+
+    var td2 = document.createElement("td")
+    var chck1 = document.createElement("input")
+    chck1.type = 'checkbox'
+    chck1.addEventListener("change", () => {
+        if (element.g1 == false) {
+            element.g1 = true
+        } else if (element.g1 == true) {
+            element.g1 = false
+        }
+    })
+    td2.appendChild(chck1)
+    tbr.append(td2)
+
+    var td3 = document.createElement("td")
+    var chck2 = document.createElement("input")
+    chck2.type = 'checkbox'
+    chck2.addEventListener("change", () => {
+        if (element.g2 == false) {
+            element.g2 = true
+        } else if (element.g2 == true) {
+            element.g2 = false
+        }
+    })
+    td3.appendChild(chck2)
+    tbr.append(td3)
+
     tableBase.appendChild(tbr)
-    
 });
 
 
+//      Mélange du tableau
 
+function shuffleArr(personnes) {
 
-//afin d'avoir plus de randomisation dans l'algo j'ai décider de faire un premmier tirage qui me permet de pas avoir le même tableau de départ pour tous les tirages
-for (i = personnes.length -1 ; i >0 ; i--) {
-    var rdm = Math.floor(Math.random() * (i+1))
-    var temp = personnes[i]
-    personnes[i] = personnes[rdm]
-    personnes[rdm] = temp
-}
-
-
-
-
-btn.addEventListener("click", () => {
-
-    //ici je vérifie si table1 a des enfants, si oui j'enclenche 2 boucles while afin de supprimer les enfants des 2 tableaux
-    if(table1.hasChildNodes){
-        while(table1.firstChild){
-            table1.removeChild(table1.lastChild)
-        }
-        while(table2.firstChild){
-            table2.removeChild(table2.lastChild)
-        }
-    }
-   
-  
-    //En ayant remarqué mes erreurs sur la v1, et après m'y être penché dessus,
-    //je me suis dis qu'il était temps de repartir à zero.
-    //la technique utilisée ici m'a permis de jouer avec le random et de retourner le tableau "randomisé"
-    //à chaque itération, je permute 2 éléments de mon tableau jusque l'avoir fait avec tout le monde 
-
-    for (i = personnes.length -1 ; i >0 ; i--) {
-        var rdm = Math.floor(Math.random() * (i+1))
+    for (i = personnes.length - 1; i > 0; i--) {
+        var rdm = Math.floor(Math.random() * (i + 1))
         var temp = personnes[i]
         personnes[i] = personnes[rdm]
         personnes[rdm] = temp
     }
+    return personnes
+}
+shuffleArr(personnes)
 
-    //ce qui simplifie pas mal pour la suite maintenant il suffit de couper notre tableau en 2 grâce à slice
-    var gr1 = personnes.slice(0, 8)
-    var gr2 = personnes.slice(8, 15)
-   
 
-    //pour finir deux petites forEach pour chaques tableaux 
-    //avec creation des tr et envois des personnes dedans et remplissage des 2 tables avec tout ceci
 
-    gr1.forEach(element => {
+
+//      Répartition des groupes
+
+var group1 = []
+var group2 = []
+
+btn.addEventListener("click", function dispoGrps() {
+
+    for (i = 0; i < personnes.length; i++) {
+        if (i % 2 == 0) {
+            if (personnes[i].g1 == false) {
+                group1.push(personnes[i])
+            } else if (personnes[i].g1 == true) {
+                group2.push(personnes[i])
+            }
+
+        } else {
+            if (personnes[i].g2 == false) {
+                group2.push(personnes[i])
+            } else if (personnes[i].g2 == true) {
+                group1.push(personnes[i])
+            }
+
+
+        }
+    }
+    //      Affichage des groupes 1 et 2
+
+
+    group1.forEach(element => {
         var tr = document.createElement("tr")
         tr.append(element.nom)
-        if(element.dispo.lundi === false || element.dispo.mercredi === false || element.dispo.vendredi === false ){
-            table2.appendChild(tr)
-        }else {
-            table1.appendChild(tr)
-        }
+        table1.appendChild(tr)
     });
 
-    gr2.forEach(element => {
+
+    group2.forEach(element => {
         var tr = document.createElement("tr")
         tr.append(element.nom)
-        if(element.dispo.mardi === false || element.dispo.jeudi === false){
-            table1.appendChild(tr)
-        }else {
-            table2.appendChild(tr)
-        }
+        table2.appendChild(tr)
     });
-})
+
+}
+)
+
+
+
+
 
 
